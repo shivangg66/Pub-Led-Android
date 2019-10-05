4ced050cf34b345bd6b424ad4e58dab74bf81d03
tanvi2601 <55015066+tanvi2601@users.noreply.github.com>
2019-10-05 09:41:53 +0530

    fragments added

---
+++ app/src/main/java/com/example/pub_led/Dashboard.java
@@ -0,0 +1,28 @@
+package com.example.pub_led;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void changeFragment(View view) {
+        Fragment fragment;
+        if (view == findViewById(R.id.button4)) {
+            fragment = new Fragment1();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.f1, fragment);
+            ft.commit();
+        }
+    }
+}
